83314524397394073163e3ba881c65b388f3a53f
AngeloOZ <53668978+AngeloOZ@users.noreply.github.com>
2020-07-08 14:23:31 -0500

    cambios prueba

--- js/carrito.js
+++ js/carrito.js
@@ -6,6 +6,29 @@ class Carrito{
             console.log('producto: ', producto);
         }
     }
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen: producto.querySelector('img').src,
+            titulo: producto.querySelector('h4').textContent,
+            precio: producto.querySelector('.precio span').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad : 1
+        }
+        this.insertarCarrito(infoProducto);   
+     }
+     insertarCarrito(producto){
+        const div = document.createElement('div');
+        div.classList.add('item') ;
+        div.innerHTML = `
+    
+                <img src="./img/producto1.jpg" alt="">
+                <div class="textos">
+                    <p class="nombre-item">Granola 200gr</p>
+                    <p>$0.99</p>
+                </div>
+        `;
+        listarProductos.appendChild(div);
+    }
     
     
     
